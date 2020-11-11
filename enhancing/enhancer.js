51aa83d3f1e917d819f609c0076f9c80f8c00333
Sean Shadle <seandshadle@gmail.com>
2020-11-11 16:43:05 -0600

    Completed MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,25 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  item.enhancement === 20 ? item.enhancement : item.enhancement++
+  return {...item};
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    item.durability -= 5;
+  } else if (item.enhancement >= 15){
+    item.durability -= 10;
+  }
+  
+  if(item.enhancement > 16){
+    item.enhancement -= 1;  }
 }
 
+return {...item};
+
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
